a3ff39cb17b9a3384452e6e32784a251f9ed5063
Seme87 <massimomonteleone@hotmail.com>
2023-02-21 13:17:25 +0100

    add footer

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -8,7 +8,44 @@ export default {
 </script>
 
 <template>
-  <footer></footer>
+  <footer class="alt-color">
+    <div class="register text-center ">
+      <h2 class="m-0" >Unisciti alla Community</h2>
+      <div class="free fw-bold my-2">It's Free!</div>
+      <p class="m-0">In qualsiasi Città c'è sempre un nuovo giocatore pronto ad indossare la maglia della tua squadra</p>
+      <a href="#" class="btn ms-btn my-3">Registrati</a>
+      <small class="d-block">Copyrights 2021 Bsoccer s.r.l</small>
+    </div>
+  </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  footer {
+    padding: 40px 0;
+   .register{
+      .free{
+        
+        font-size: 50px;
+        color: #E0154A;
+      }
+
+      h4, h2{
+        color: white;
+      }
+
+      small, p {
+        color: grey;
+      }
+
+      .ms-btn{
+        padding: 10px 60px;
+        border-radius: 15px;
+        background-color: white;
+
+      }
+
+
+   }
+    
+  }
+</style>
