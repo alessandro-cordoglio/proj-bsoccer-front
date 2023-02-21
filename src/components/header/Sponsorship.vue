e161f6cadce1dfa1936089d17faf1b335b81144c
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-02-21 15:58:45 +0100

    inserito background

--- src/components/header/Sponsorship.vue
+++ src/components/header/Sponsorship.vue
@@ -8,9 +8,26 @@
 </script>
 
 <template>
-
+    <div class="background_image relative">
+        <div class="img_container absolute">
+            <img src="src/assets/SponsorWallpaper.jpg" alt="">
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.background_image{
+    .img_container{
+        top: 0; bottom: 0; left: 0; right: 0; 
+        filter: brightness(0.4);
+        pointer-events: none;
+        z-index: -200;
+        img{
+            width: 100%;
+            
+            
+        }
+    }
 
+}
 </style>
