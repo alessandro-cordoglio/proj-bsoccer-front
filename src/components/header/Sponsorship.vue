c2fd8ded9fb79eaea2819f8b7cae41649d907330
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-02-21 17:29:46 +0100

    add: container carousel + hero title

--- src/components/header/Sponsorship.vue
+++ src/components/header/Sponsorship.vue
@@ -16,6 +16,9 @@ import PlayerCard from "../commons/PlayerCard.vue";
         <div class="img_container">
             <img src="src/assets/SponsorWallpaper.jpg" alt="">
         </div>
+        <div class="hero_title absolute">
+            <img src="https://fontmeme.com/permalink/230221/f085f5f243c0fbdec578539547ed3ba6.png" alt="ea-sports-font" border="0">
+        </div>
         <div class="carousel_card absolute">
                 <PlayerCard />    
         </div>
@@ -34,8 +37,16 @@ import PlayerCard from "../commons/PlayerCard.vue";
             width: 100%;
         }
     }
+    .hero_title{
+        top:20px; bottom: 0; left: 580px; right: 0;
+    }
     .carousel_card{
-        top:0; bottom: 0; left: 0; right: 0;
+        border: 2px solid crimson;
+        border-radius: 10px;
+        width: 1000px;
+        height: 600px;
+        background-color: white;
+        top:100px; bottom: 0; left: 450px; right: 0;
     }
 
 }
