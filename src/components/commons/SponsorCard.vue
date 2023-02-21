4c882b2ef84afc1fc3b3c8900c21f54627df1dc0
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-02-21 21:32:09 +0100

    creata card x carosello

---
+++ src/components/commons/SponsorCard.vue
@@ -0,0 +1,85 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="item d-flex">
+          <div class="item_img">
+              <img src="src/assets/placeholder.png" alt="">
+          </div>
+          <div class="item_text">
+              <div class="item_name">
+                  <h1 class="fw-bold">MARCO CAZZI</h1>
+              </div>
+              <div class="item_bottom">
+                  <div class="item_role d-flex align-items-center">
+                      <h2>ATTACCANTE</h2>
+                  </div>
+                  <div class="item_star">
+                      <i class="fa-solid fa-star"></i>
+                      <i class="fa-solid fa-star"></i>
+                      <i class="fa-solid fa-star-half-stroke"></i>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.item{
+    width: 500px;
+    height: 200px;
+    .item_img{
+        margin-right: 10px ;
+        position: relative;
+        border-radius: 50%;
+        background-color: white;
+        width: 55px;
+        height: 50px;
+        text-align: center;
+        img{
+            padding: 5px;
+            width: 40px;
+        }
+    }
+    .item_text{
+        height: fit-content;
+        background-color: white;
+        width: 100%;
+        border-radius: 0 20px 0px 20px;
+        .item_name{
+            padding: 10px;
+            border-bottom: 3px solid goldenrod;
+            background:linear-gradient(to right, #BF953F, #FCF6BA, #B38728, #FBF5B7, #AA771C);
+        }
+        .item_bottom{
+            height: 100%;
+            background-color: crimson;
+            border-radius: 0 0 0 20px;
+            border-right: 2px solid crimson ;
+            border-left: 2px solid crimson ;
+            border-bottom: 2px solid crimson ;
+            .item_role{
+                padding-left: 10px;
+                padding-top: 5px;
+            }
+            .item_star{
+                padding-left: 20px;
+                padding-top: 20px ;
+                i{
+                    color: goldenrod;
+                    font-size: 25px;
+                }
+            }
+
+        }
+    }
+    
+}
+
+
+</style>
--- src/components/header/Sponsorship.vue
+++ src/components/header/Sponsorship.vue
@@ -1,9 +1,11 @@
 <script>
+import SponsorCard from "../commons/SponsorCard.vue";
 import PlayerCard from "../commons/PlayerCard.vue";
     export default {
         name: "Sponsorship",
         components: {
             PlayerCard,
+            SponsorCard,
         },
         data() {
             return {};
@@ -20,7 +22,15 @@ import PlayerCard from "../commons/PlayerCard.vue";
             <img src="https://fontmeme.com/permalink/230221/f085f5f243c0fbdec578539547ed3ba6.png" alt="ea-sports-font" border="0">
         </div>
         <div class="carousel_card absolute">
-                <PlayerCard />    
+            <div class="sponsor_card">
+                <SponsorCard/>
+            </div>
+            <div class="next">
+                <i class="fa-solid fa-circle-chevron-right"></i>
+            </div>
+            <div class="prev">
+                <i class="fa-solid fa-circle-chevron-left"></i>
+            </div>
         </div>
     </div>
 </template>
@@ -39,14 +49,35 @@ import PlayerCard from "../commons/PlayerCard.vue";
     }
     .hero_title{
         top:20px; bottom: 0; left: 580px; right: 0;
+        width: fit-content;
+        height: fit-content;
     }
     .carousel_card{
-        border: 2px solid crimson;
         border-radius: 10px;
         width: 1000px;
         height: 600px;
-        background-color: white;
-        top:100px; bottom: 0; left: 450px; right: 0;
+        top:0; bottom: 100px; left: 450px; right: 0;
+        .sponsor_card{
+            position: absolute;
+            top: 150px; bottom: 0; left: 200px; right: 0;
+            width: min-content;
+            height: min-content;
+        }
+        .prev, .next {
+            max-width: fit-content;
+            margin: 10px 0;
+            position: absolute;
+            cursor: pointer;
+            z-index: 40;
+            top: 400px;
+            i{
+                font-size: 30px;
+                color: lightcoral;
+            }
+        }
+        .next{
+            right: 0px;
+        }
     }
 
 }
