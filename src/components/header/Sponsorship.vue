48c4ebaf1c35761930b5d0de55ae528950c30d4d
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-02-21 16:28:01 +0100

    fix hero

--- src/components/header/Sponsorship.vue
+++ src/components/header/Sponsorship.vue
@@ -1,6 +1,10 @@
 <script>
+import PlayerCard from "../commons/PlayerCard.vue";
     export default {
         name: "Sponsorship",
+        components: {
+            PlayerCard,
+        },
         data() {
             return {};
         },
@@ -9,14 +13,18 @@
 
 <template>
     <div class="background_image relative">
-        <div class="img_container absolute">
+        <div class="img_container">
             <img src="src/assets/SponsorWallpaper.jpg" alt="">
+        </div>
+        <div class="carousel_card absolute">
+                <PlayerCard />    
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .background_image{
+    max-height: 100%;
     .img_container{
         top: 0; bottom: 0; left: 0; right: 0; 
         filter: brightness(0.4);
@@ -24,9 +32,10 @@
         z-index: -200;
         img{
             width: 100%;
-            
-            
         }
+    }
+    .carousel_card{
+        top:0; bottom: 0; left: 0; right: 0;
     }
 
 }
