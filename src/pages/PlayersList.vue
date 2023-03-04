1074ed2f636355198c9d6528866ac7531f1a9622
RaimondoCosma <raimondo.cosma@gmail.com>
2023-03-04 16:04:31 +0100

    Aggiunge scroll up al click

--- src/pages/PlayersList.vue
+++ src/pages/PlayersList.vue
@@ -18,103 +18,126 @@ export default {
     } else {
       this.getPlayers();
     }
-    
   },
-  computed: {
-   
-  },
+  computed: {},
   methods: {
-
+    scrollToTop() {
+      let allPlayers = document.querySelector(".all-players");
+      allPlayers.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
     isNextDisabled() {
-      return this.store.currentPage === this.store.lastPage || this.store.currentPage > this.store.lastPage
+      return (
+        this.store.currentPage === this.store.lastPage ||
+        this.store.currentPage > this.store.lastPage
+      );
     },
     getPlayers() {
       this.store.currentPage = 1;
-      axios.get(`http://127.0.0.1:8000/api/players?page=${this.store.currentPage}`, {
-        params: {
-          page: this.store.currentPage,
-          perPage: this.store.lastPage
-        }
-      })
-      .then(res => {
-        this.store.lastPage= res.data.last_page;
-        console.log(res.data);
-        this.store.totalPlayers= res.data.total; 
-        this.store.players = res.data.data;
-        console.log(res.data.total)
-      })
-      .catch(error => {
-        console.log(error);
-      })
-      
+      axios
+        .get(
+          `http://127.0.0.1:8000/api/players?page=${this.store.currentPage}`,
+          {
+            params: {
+              page: this.store.currentPage,
+              perPage: this.store.lastPage,
+            },
+          }
+        )
+        .then((res) => {
+          this.store.lastPage = res.data.last_page;
+          console.log(res.data);
+          this.store.totalPlayers = res.data.total;
+          this.store.players = res.data.data;
+          console.log(res.data.total);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
 
     getPlayersButton() {
-      axios.get(`http://127.0.0.1:8000/api/players?page=${this.store.currentPage}`, {
-        params: {
-          page: this.store.currentPage,
-          perPage: this.store.lastPage
-        }
-      })
-      .then(res => {
-        this.store.lastPage= res.data.last_page;
-        console.log(res.data);
-        this.store.totalPlayers= res.data.total; 
-        this.store.players = res.data.data;
-        console.log(res.data.total)
-      })
-      .catch(error => {
-        console.log(error);
-      })
+      axios
+        .get(
+          `http://127.0.0.1:8000/api/players?page=${this.store.currentPage}`,
+          {
+            params: {
+              page: this.store.currentPage,
+              perPage: this.store.lastPage,
+            },
+          }
+        )
+        .then((res) => {
+          this.store.lastPage = res.data.last_page;
+          console.log(res.data);
+          this.store.totalPlayers = res.data.total;
+          this.store.players = res.data.data;
+          console.log(res.data.total);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-
 
     changePage(num) {
       this.store.currentPage += num;
+      this.scrollToTop();
       if (this.store.selectedRole) {
         this.getPlayersByRoleButton();
       } else {
         this.getPlayersButton();
-  }
+      }
     },
 
     getPlayersByRole() {
       this.store.currentPage = 1;
-      axios.get(`http://127.0.0.1:8000/api/players?page=${this.store.currentPage}`, {
-          params: {
-            role: this.store.selectedRole,
-            page: this.store.currentPage,
-            perPage: this.store.lastPage
+      axios
+        .get(
+          `http://127.0.0.1:8000/api/players?page=${this.store.currentPage}`,
+          {
+            params: {
+              role: this.store.selectedRole,
+              page: this.store.currentPage,
+              perPage: this.store.lastPage,
+            },
           }
-        })
-        .then(res => {
-          console.log(res.data)
-          this.store.lastPage= res.data.last_page;
+        )
+        .then((res) => {
+          console.log(res.data);
+          this.store.lastPage = res.data.last_page;
           this.store.players = res.data.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
-        })
-        
+        });
     },
     getPlayersByRoleButton() {
-      axios.get(`http://127.0.0.1:8000/api/players?page=${this.store.currentPage}`, {
-          params: {
-            role: this.store.selectedRole,
-            page: this.store.currentPage,
-            perPage: this.store.lastPage
+      axios
+        .get(
+          `http://127.0.0.1:8000/api/players?page=${this.store.currentPage}`,
+          {
+            params: {
+              role: this.store.selectedRole,
+              page: this.store.currentPage,
+              perPage: this.store.lastPage,
+            },
           }
-        })
-        .then(res => {
-          console.log(res.data)
-          this.store.lastPage= res.data.last_page;
+        )
+        .then((res) => {
+          console.log(res.data);
+          this.store.lastPage = res.data.last_page;
           this.store.players = res.data.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
-        })
+        });
     },
-    
   },
 };
 </script>
@@ -126,27 +149,40 @@ export default {
         v-for="(player, index) in this.store.players"
         :data="player"
         class="players-list"
-        :key="player.id "
+        :key="player.id"
       />
     </div>
     <div class="ms_bt_container">
-        <button class="btn me-4 btn-info" type="button" :disabled="store.currentPage === 1" @click="changePage(-1)">-- Prev</button>
-        <button class="btn me-4 btn-info" type="button" :disabled = "isNextDisabled()" @click="changePage(1)">-- Next</button>
+      <button
+        class="btn me-4 btn-info"
+        type="button"
+        :disabled="store.currentPage === 1"
+        @click="changePage(-1)"
+      >
+        -- Prev
+      </button>
+      <button
+        class="btn me-4 btn-info"
+        type="button"
+        :disabled="isNextDisabled()"
+        @click="changePage(1)"
+      >
+        -- Next
+      </button>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
-
-.ms_bt_container{
-  padding-top: 30px ;
-  padding-bottom: 30px ;
+.ms_bt_container {
+  padding-top: 30px;
+  padding-bottom: 30px;
   display: flex;
   justify-content: center;
 }
 .all-players::-webkit-scrollbar {
-    display: none;
-  }
+  display: none;
+}
 .all-players {
   background-image: url("http://www.asdbassabresciana.it/wp-content/uploads/2016/06/Sfondo-HD-calcio-sport.jpeg");
   max-height: 100vh;
